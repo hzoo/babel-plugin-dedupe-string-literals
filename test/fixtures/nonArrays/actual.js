18c29d3a431564884a8e13b03e2357f6d261d4fd
Henry Zhu <hi@henryzoo.com>
2016-05-24 06:41:33 -0400

    replace all StringLiterals rather than just only in arrays, account f… (#4)
    * replace all StringLiterals rather than just only in arrays, account for import/export
    
    * test travis
    
    * test case for 'use strict';

---
+++ test/fixtures/nonArrays/actual.js
@@ -0,0 +1,27 @@
+'use strict';
+
+import z from 'a';
+
+var a = ['a','a'];
+
+function b(c = 'a') {
+  'use strict';
+  let d = ['a','a'];
+
+  e(() => {
+    var f = ['a','a'];
+  });
+
+  if ('a') {
+
+  }
+}
+
+g('a');
+h['a']
+const i = {
+  j: 'a',
+  ['a']: 'a'
+};
+
+k = true && 'a';
---
+++ test/fixtures/nonArrays/expected.js
@@ -0,0 +1,27 @@
+'use strict';
+
+import z from 'a';
+
+var _a = 'a';
+var a = [_a, _a];
+
+function b(c = _a) {
+  'use strict';
+
+  let d = [_a, _a];
+
+  e(() => {
+    var f = [_a, _a];
+  });
+
+  if (_a) {}
+}
+
+g(_a);
+h[_a];
+const i = {
+  j: _a,
+  [_a]: _a
+};
+
+k = true && _a;
